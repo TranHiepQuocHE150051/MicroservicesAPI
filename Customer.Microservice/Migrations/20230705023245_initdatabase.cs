df751983b7e54ad473758364caee1db8e00b2601
TrầnHiệpQuốcHE150051 <91178389+TranHiepQuocHE150051@users.noreply.github.com>
2023-07-11 14:30:06 +0700

    Microservice API

---
+++ Customer.Microservice/Migrations/20230705023245_initdatabase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Customer.Microservice.Migrations
+{
+    public partial class initdatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
